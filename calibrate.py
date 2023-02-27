0e4d0ccf1873b5103d01e2d4900ba4ec42314cd7
mrkotee <mrkotee08@ya.ru>
2023-02-27 13:36:14 +0300

    feature(Calibrate): finish calibrate script

--- calibrate.py
+++ calibrate.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import cv2
 import json
+import dataclasses
 from statistics import mean
 
 from config import Config
@@ -11,7 +12,7 @@ from lepton import Lepton
 def main(conf: Config):
     raws = []
     results = {}
-    lepton_cam = Lepton(conf.spi_device, conf.i2c_device, conf.i2c_addr)
+    lepton_cam = Lepton(conf.spi_device, conf.i2c_device, conf.i2c_addr, 0, 0)
     while 1:
         while 1:
             target_temp = input("Введите температуру (напр. 36.6) измеряемого предмета: ")
@@ -24,6 +25,8 @@ def main(conf: Config):
         cam_temp = lepton_cam.lepton_i2c.get_cam_temp()
         raw = lepton_cam._capture()
 
+        print(f"{cam_temp=:.2}")
+
         min_max_raw = (min([j[0] for i in raw[int(len(raw) / 2) - 2:int(len(raw) / 2) + 2] for j in
                             i[int(len(i) / 2) - 2:int(len(i) / 2) + 2]]),
                        max([j[0] for i in raw[int(len(raw) / 2) - 2:int(len(raw) / 2) + 2] for j in
@@ -35,53 +38,17 @@ def main(conf: Config):
         if len(raws) == 1:
             continue
 
-        for t, r, c_t in raws:
-            results[t] = []
-            a = 0.00001
-            # T = a·RAW + T_C - k
-            while a < 0.1:
-                a += 0.00001
-                res = a * r
-                if res - c_t < 0:
-                    a += 0.0001
-                    continue
-                if res + c_t - t < 500:
-                    k = res + c_t - t
-                    results[t].append((a, k))
-        if not len(results):
-            continue
-        print(f"len results = {sum([len(v) for v in results.values()])}")
-        means = []
-        results2 = []
-        for _ in range(len(results)):
-            t, ress = results.popitem()
-            for a1, k1 in ress:
-                a1, k1 = round(a1, 4), round(k1, 1)
-                for ress2 in results.values():
-                    for a2, k2 in ress2:
-                        if round(a2, 4) != a1:
-                            continue
-                        if round(k2, 1) != k1:
-                            continue
-                        results2.append((a2, k2))
-        print(f"{len(results2)=}")
-        # print(f"{len(means)=} {means=}")
+        left = np.array([[raws[0][1], -1], [raws[1][1], -1]])
+        right = np.array([raws[0][0]-raws[0][2], raws[1][0]-raws[1][2]])
+        a, k = np.linalg.inv(left).dot(right)
 
-        if len(results2):
-            print(f"{len(results2)=} {results2[0]=}")
-            with open("res.json", 'w') as f:
-                json.dump(results2, f)
-            break
+        print(f"{a=} {k=}")
 
+        conf.a_coefficient = a
+        conf.k_coefficient = k
+        with open("config.json", 'w') as f:
+            json.dump(dataclasses.asdict(conf), f)
 
-# means = [(0.03177, 254.24802865690444), (0.03361, 250.32532149317547), (0.031735, 250.1941263879817),
-#          (0.031735, 250.1941263879817), (0.03361, 250.32532149317547), (0.03361, 250.32532149317547)]
-
-# results = [(0.0028799999999999997, 0.8607999999999976), (0.0028799999999999997, 0.9080800000000266),
-#            (0.0028799999999999997, 0.8607999999999976), (0.0028799999999999997, 0.9080800000000266),
-#            (0.0028799999999999997, 0.8523200000000415), (0.0028799999999999997, 0.8607999999999976),
-#            (0.0028799999999999997, 0.9080800000000266), (0.0028799999999999997, 0.8523200000000415),
-#            (0.0028799999999999997, 0.4064000000000263), (0.0010100000000000003, 0.5245200000000239)]
 
 if __name__ == '__main__':
     with open("config.json") as f:
